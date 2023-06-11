6f6a4ca5622c659288d085867246cfd36576ace2
Jannik-LS <70889142+Jannik-LS@users.noreply.github.com>
2023-06-11 18:35:56 +0200

    Initial commit

---
+++ src/main/java/technikum/at/jannikschwerdtner_abschlusspruefungslm/JannikSchwerdtnerAbschlusspruefungSlmApplication.java
@@ -0,0 +1,13 @@
+package technikum.at.jannikschwerdtner_abschlusspruefungslm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JannikSchwerdtnerAbschlusspruefungSlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JannikSchwerdtnerAbschlusspruefungSlmApplication.class, args);
+    }
+
+}
